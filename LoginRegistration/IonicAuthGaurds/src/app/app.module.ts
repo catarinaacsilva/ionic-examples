000da9c84c58d29c5adad4183ae33c63ce4edddd
Catarina <c.alexandracorreia@ua.pt>
2020-08-25 10:28:48 +0100

    Update app module ts

--- LoginRegistration/IonicAuthGaurds/src/app/app.module.ts
+++ LoginRegistration/IonicAuthGaurds/src/app/app.module.ts
@@ -9,13 +9,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+import { IonicStorageModule } from '@ionic/storage';
+import { AuthGuard } from './services/AuthGuard.service';
+import { AuthenticationService } from './services/Authentication.service';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
+    AuthGuard,
+    AuthenticationService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
